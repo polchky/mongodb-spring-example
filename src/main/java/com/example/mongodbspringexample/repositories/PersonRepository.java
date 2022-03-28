a1bc5f1ede6ac239ff59cdaa95315132f96ff6f3
polchky <polchky@gmail.com>
2022-03-28 16:59:33 +0200

    Add person queries

--- src/main/java/com/example/mongodbspringexample/repositories/PersonRepository.java
+++ src/main/java/com/example/mongodbspringexample/repositories/PersonRepository.java
@@ -2,6 +2,21 @@ package com.example.mongodbspringexample.repositories;
 
 import com.example.mongodbspringexample.domain.Person;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface PersonRepository extends MongoRepository<Person, String> {
+
+    Person findByFirstName(String firstName);
+
+    List<Person> findByLastName(String lastName);
+
+    List<Person> findByFirstNameAndLastName(String firstName, String lastName);
+
+    @Query("{'address.city.name': ?0}")
+    List<Person> findByCity(String city);
+
+    List<Person> findByAddressCityName(String name);
+
 }
